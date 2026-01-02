c5d1890cf779ffd75640c97f93582dfc7e6b395f
Narendra Darel <narendra.darelrafi@gmail.com>
2026-01-02 23:59:29 +0700

    welcome blade fixing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>KASIO - POS Retail Indonesia</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/css/app.css', 'resources/js/app.js')
 </head>
 <body class="antialiased font-sans bg-white dark:bg-slate-900 text-black dark:text-white">
     
