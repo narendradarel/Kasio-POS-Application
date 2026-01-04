9100538d7cbb11ec275dad9c874ad4db987fc664
narendradarel <146500653+narendradarel@users.noreply.github.com>
2026-01-05 00:39:52 +0700

    Add CSRF token validation exception for midtrans webhook

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\TrackVisitors::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+        'midtrans/webhook', 
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
